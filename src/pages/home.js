9e7e03bdde933eb7cb8b11c2d1a50186e8339c51
LightDDark <myavraham25@gmail.com>
2024-05-16 00:52:48 +0300

    Added Grid items to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,6 +11,8 @@ import {
 } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import fantasyImg from '../static/bookCategories/fantasy-israel.jpg'
+import romanceImg from '../static/bookCategories/romance-israel.jpg'
+import mysteryImg from '../static/bookCategories/mystery-israel.jpg'
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2),
@@ -44,6 +46,36 @@ export default function Home() {
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
                   Swords, Might and Magic !
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card>
+            <CardActionArea>
+              <StyledCardMedia image={romanceImg} title="Fantasy" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Romance
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {'Love & Butterflies <3'}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card>
+            <CardActionArea>
+              <StyledCardMedia image={mysteryImg} title="Fantasy" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Mystery
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {'The unknown is intriguing....and scary'}
                 </Typography>
               </CardContent>
             </CardActionArea>
